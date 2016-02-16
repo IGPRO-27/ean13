b6a12ac110034d0d35ecbd2d103c5a7477d9ac3c
Adel27 <r-27adel@live.fr>
2016-02-16 09:44:10 +0100

    Refactoring code
    Remove required constraint on fields S & L

--- ean13.py
+++ ean13.py
@@ -5,16 +5,16 @@ from openerp import models, fields, api, exceptions
 class ean13project(models.Model):
     _inherit = 'product.template'
 
-    ean13 = fields.Char( required=True)
-    default_code = fields.Char(required=True)
+    # ean13 = fields.Char( required=True)
+    # default_code = fields.Char(required=True)
 
 
 
 class Ean13(models.Model):
     _inherit = 'product.product'
 
-    ean13 = fields.Char(string ="Réference S" ,  required=True)
-    default_code = fields.Char(string ="Réference L" ,  required=True)
+    ean13 = fields.Char(string ="Réference S")
+    default_code = fields.Char(string ="Réference L")
 
     def _check_ean_key(self, cr, uid, ids, context=None):
         return True
